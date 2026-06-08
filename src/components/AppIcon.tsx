0f25524c4b1fe602ccf7058fd6055770bf153dc9
niemisami <sami@nieminen.fi>
2026-06-09 00:08:01 +0300

    Add karttu AppIcon

---
+++ src/components/AppIcon.tsx
@@ -0,0 +1,39 @@
+import { createLucideIcon } from 'lucide-react'
+
+export const AppIcon = createLucideIcon('appIcon', [
+  [
+    'path',
+    {
+      d: 'm13.184 5.2 3.5-3.5s1.568-1.465 3 0 0 3 0 3l-3.5 3.5',
+      key: '1iqmij',
+    },
+  ],
+  [
+    'path',
+    {
+      d: 'm14.684 14.7 4 4',
+      key: '4ls596',
+    },
+  ],
+  [
+    'path',
+    {
+      d: 'M16.184 13.2 4.684 1.7s-1.461-1.575-3 0 0 3 0 3l11.5 11.5z',
+      key: 'xhhllm',
+    },
+  ],
+  [
+    'path',
+    {
+      d: 'm4.178 13.212 3 2.99',
+      key: 'd0ilvl',
+    },
+  ],
+  [
+    'path',
+    {
+      d: 'm5.684 15.7-3 3',
+      key: 'yb1iau',
+    },
+  ],
+])
