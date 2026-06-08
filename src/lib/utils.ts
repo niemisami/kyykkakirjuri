c43315de9a5ec35c8c00914a64bd0c8fbe621422
niemisami <sami@nieminen.fi>
2026-06-08 09:34:39 +0300

    Add pluralize

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -5,3 +5,10 @@ import { twMerge } from 'tailwind-merge'
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+const fiCardinalRules = new Intl.PluralRules('fi-FI')
+
+export function pluralize(count: number, singular: string, plural: string) {
+  const pluralForm = fiCardinalRules.select(count) === 'one' ? singular : plural
+  return `${count} ${pluralForm}`
+}
